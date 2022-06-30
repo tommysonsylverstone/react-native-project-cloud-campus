9927284f7312fedfdb158327ce1e28d9ff2e0242
btenreiro <tenreiro.benjamin@gmail.com>
2022-06-30 10:35:48 +0200

    added components

---
+++ components/navigation/NavBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text } from "react-native";
+
+export default class NavBar extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <Text>NavBar</Text>
+        )
+    }
+}
